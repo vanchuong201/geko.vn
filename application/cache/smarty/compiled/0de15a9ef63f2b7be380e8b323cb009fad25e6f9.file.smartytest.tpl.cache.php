fd2fa5016c5db9db8c3ce0dc7de10db08b4c5a15
Chuong Nguyen <chuongnguyenvan@muare.vn>
2015-01-24 23:21:37 +0700

    up code

---
+++ application/cache/smarty/compiled/0de15a9ef63f2b7be380e8b323cb009fad25e6f9.file.smartytest.tpl.cache.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version Smarty-3.1.17, created on 2015-01-24 15:17:32
+         compiled from "application\views\smartytest.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:154c37f4c5262d0-64805914%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0de15a9ef63f2b7be380e8b323cb009fad25e6f9' => 
+    array (
+      0 => 'application\\views\\smartytest.tpl',
+      1 => 1400575504,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '154c37f4c5262d0-64805914',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'body' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.17',
+  'unifunc' => 'content_54c37f4c55f418_58807341',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54c37f4c55f418_58807341')) {function content_54c37f4c55f418_58807341($_smarty_tpl) {?><html>
+<head>
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<body>
+    <?php echo $_smarty_tpl->tpl_vars['body']->value;?>
+
+</body>
+</html><?php }} ?>
---
+++ application/cache/smarty/compiled/da3d53ef4edfd4f5b43f496d3f62b13007a39ab6.file.inheritancetest.tpl.cache.php
@@ -0,0 +1,41 @@
+<?php /* Smarty version Smarty-3.1.17, created on 2015-01-24 12:35:09
+         compiled from "application\views\inheritancetest.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2020554c3836d84a443-82447970%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'da3d53ef4edfd4f5b43f496d3f62b13007a39ab6' => 
+    array (
+      0 => 'application\\views\\inheritancetest.tpl',
+      1 => 1400575504,
+      2 => 'file',
+    ),
+    '2f8e48cf519b94f283095532e0b7015cfed14ff5' => 
+    array (
+      0 => 'application\\views\\master.tpl',
+      1 => 1400575504,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2020554c3836d84a443-82447970',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.17',
+  'unifunc' => 'content_54c3836d8a72c5_26161111',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54c3836d8a72c5_26161111')) {function content_54c3836d8a72c5_26161111($_smarty_tpl) {?><html>
+<head>
+    <title>
+    <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+
+</title>
+</head>
+<body>
+
+    <?php echo $_smarty_tpl->tpl_vars['body']->value;?>
+
+
+</body>
+</html><?php }} ?>
