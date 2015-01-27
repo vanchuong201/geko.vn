23277ae2f69f6cebd67393dca4b6a800b1286ae5
vanchuong201 <butterfly184>
2015-01-27 08:02:13 +0700

    123

--- application/cache/smarty/compiled/0de15a9ef63f2b7be380e8b323cb009fad25e6f9.file.smartytest.tpl.cache.php
+++ application/cache/smarty/compiled/0de15a9ef63f2b7be380e8b323cb009fad25e6f9.file.smartytest.tpl.cache.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.17, created on 2015-01-24 15:17:32
+<?php /* Smarty version Smarty-3.1.17, created on 2015-01-26 17:41:03
          compiled from "application\views\smartytest.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:154c37f4c5262d0-64805914%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0de15a9ef63f2b7be380e8b323cb009fad25e6f9' => 
     array (
       0 => 'application\\views\\smartytest.tpl',
-      1 => 1400575504,
+      1 => 1422169580,
       2 => 'file',
     ),
   ),
@@ -15,14 +15,15 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'cache_lifetime' => 3600,
+  'version' => 'Smarty-3.1.17',
+  'unifunc' => 'content_54c37f4c55f418_58807341',
   'variables' => 
   array (
     'title' => 0,
     'body' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.17',
-  'unifunc' => 'content_54c37f4c55f418_58807341',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_54c37f4c55f418_58807341')) {function content_54c37f4c55f418_58807341($_smarty_tpl) {?><html>
 <head>
