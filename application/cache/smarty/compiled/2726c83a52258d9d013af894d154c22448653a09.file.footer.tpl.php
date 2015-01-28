909b4117c6cc06ad32f04ff73527ca99785f7b1f
Chuong Nguyen <chuongnguyenvan@muare.vn>
2015-01-28 17:56:29 +0700

    312312312

---
+++ application/cache/smarty/compiled/2726c83a52258d9d013af894d154c22448653a09.file.footer.tpl.php
@@ -0,0 +1,313 @@
+<?php /* Smarty version Smarty-3.1.17, created on 2015-01-28 04:00:55
+         compiled from "application\views\Footer\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2666254c5fb420a3c40-18038324%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '2726c83a52258d9d013af894d154c22448653a09' => 
+    array (
+      0 => 'application\\views\\Footer\\footer.tpl',
+      1 => 1422354042,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2666254c5fb420a3c40-18038324',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.17',
+  'unifunc' => 'content_54c5fb420e45d1_46478779',
+  'variables' => 
+  array (
+    'base_url' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54c5fb420e45d1_46478779')) {function content_54c5fb420e45d1_46478779($_smarty_tpl) {?>    <!-- BEGIN STEPS -->
+    <div class="steps3 steps3red">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 steps3-col">
+                    <i class="fa fa-truck"></i>
+                    <div>
+                        <h2>Free shipping</h2>
+                        <em>Express delivery withing 3 days</em>
+                    </div>
+                    <span>&nbsp;</span>
+                </div>
+                <div class="col-md-4 steps3-col">
+                    <i class="fa fa-gift"></i>
+                    <div>
+                        <h2>Daily Gifts</h2>
+                        <em>3 Gifts daily for lucky customers</em>
+                    </div>
+                    <span>&nbsp;</span>
+                </div>
+                <div class="col-md-4 steps3-col">
+                    <i class="fa fa-phone"></i>
+                    <div>
+                        <h2>477 505 8877</h2>
+                        <em>24/7 customer care available</em>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END STEPS -->
+
+    <!-- BEGIN PRE-FOOTER -->
+    <div class="pre-footer">
+        <div class="container">
+            <div class="row">
+                <!-- BEGIN BOTTOM ABOUT BLOCK -->
+                <div class="col-md-3 col-sm-6 pre-footer-col">
+                    <h2>About us</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam sit nonummy nibh euismod tincidunt ut laoreet dolore magna aliquarm erat sit volutpat. Nostrud exerci tation ullamcorper suscipit lobortis nisl aliquip  commodo consequat. </p>
+                    <p>Duis autem vel eum iriure dolor vulputate velit esse molestie at dolore.</p>
+                </div>
+                <!-- END BOTTOM ABOUT BLOCK -->
+                <!-- BEGIN BOTTOM INFO BLOCK -->
+                <div class="col-md-3 col-sm-6 pre-footer-col">
+                    <h2>Information</h2>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Delivery Information</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Customer Service</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Order Tracking</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Shipping & Returns</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="contacts.html">Contact Us</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Careers</a></li>
+                        <li><i class="fa fa-angle-right"></i> <a href="#">Payment Methods</a></li>
+                    </ul>
+                </div>
+                <!-- END INFO BLOCK -->
+                <!-- BEGIN TWITTER BLOCK -->
+                <div class="col-md-3 col-sm-6 pre-footer-col">
+                    <h2>Latest Tweets</h2>
+                    <dl class="dl-horizontal f-twitter">
+                        <dt><i class="fa fa-twitter"></i></dt>
+                        <dd>
+                            <a href="#">@keenthemes</a>
+                            Imperdiet condimentum diam dolor lorem sit consectetur adipiscing
+                            <span>3 min ago</span>
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal f-twitter">
+                        <dt><i class="fa fa-twitter"></i></dt>
+                        <dd>
+                            <a href="#">@keenthemes</a>
+                            Imperdiet condimentum diam dolor lorem sit consectetur adipiscing
+                            <span>3 min ago</span>
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal f-twitter">
+                        <dt><i class="fa fa-twitter"></i></dt>
+                        <dd>
+                            <a href="#">@keenthemes</a>
+                            Imperdiet condimentum diam dolor lorem sit consectetur adipiscing
+                            <span>3 min ago</span>
+                        </dd>
+                    </dl>
+                </div>
+                <!-- END TWITTER BLOCK -->
+                <!-- BEGIN BOTTOM CONTACTS -->
+                <div class="col-md-3 col-sm-6 pre-footer-col">
+                    <h2>Our Contacts</h2>
+                    <address class="margin-bottom-40">
+                        35, Lorem Lis Street, Park Ave<br>
+                        California, US<br>
+                        Phone: 300 323 3456<br>
+                        Fax: 300 323 1456<br>
+                        Email: <a href="mailto:info@metronic.com">info@metronic.com</a><br>
+                        Skype: <a href="skype:metronic">metronic</a>
+                    </address>
+                </div>
+                <!-- END BOTTOM CONTACTS -->
+            </div>
+            <hr>
+            <div class="row">
+                <!-- BEGIN SOCIAL ICONS -->
+                <div class="col-md-6 col-sm-6">
+                    <ul class="social-icons">
+                        <li><a class="rss" data-original-title="rss" href="#"></a></li>
+                        <li><a class="facebook" data-original-title="facebook" href="#"></a></li>
+                        <li><a class="twitter" data-original-title="twitter" href="#"></a></li>
+                        <li><a class="googleplus" data-original-title="googleplus" href="#"></a></li>
+                        <li><a class="linkedin" data-original-title="linkedin" href="#"></a></li>
+                        <li><a class="youtube" data-original-title="youtube" href="#"></a></li>
+                        <li><a class="vimeo" data-original-title="vimeo" href="#"></a></li>
+                        <li><a class="skype" data-original-title="skype" href="#"></a></li>
+                    </ul>
+                </div>
+                <!-- END SOCIAL ICONS -->
+                <!-- BEGIN NEWLETTER -->
+                <div class="col-md-6 col-sm-6">
+                    <div class="pre-footer-subscribe-box pull-right">
+                        <h2>Newsletter</h2>
+                        <form action="#">
+                            <div class="input-group">
+                                <input type="text" placeholder="youremail@mail.com" class="form-control">
+                      <span class="input-group-btn">
+                        <button class="btn btn-primary" type="submit">Subscribe</button>
+                      </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- END NEWLETTER -->
+            </div>
+        </div>
+    </div>
+    <!-- END PRE-FOOTER -->
+
+    <!-- BEGIN FOOTER -->
+    <div class="footer padding-top-15">
+        <div class="container">
+            <div class="row">
+                <!-- BEGIN COPYRIGHT -->
+                <div class="col-md-6 col-sm-6 padding-top-10">
+                    2014 © Metronic Shop UI. ALL Rights Reserved.
+                </div>
+                <!-- END COPYRIGHT -->
+                <!-- BEGIN PAYMENTS -->
+                <div class="col-md-6 col-sm-6">
+                    <ul class="list-unstyled list-inline pull-right margin-bottom-15">
+                        <li><img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/img/payments/western-union.jpg" alt="We accept Western Union" title="We accept Western Union"></li>
+                        <li><img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/img/payments/american-express.jpg" alt="We accept American Express" title="We accept American Express"></li>
+                        <li><img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/img/payments/MasterCard.jpg" alt="We accept MasterCard" title="We accept MasterCard"></li>
+                        <li><img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/img/payments/PayPal.jpg" alt="We accept PayPal" title="We accept PayPal"></li>
+                        <li><img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/img/payments/visa.jpg" alt="We accept Visa" title="We accept Visa"></li>
+                    </ul>
+                </div>
+                <!-- END PAYMENTS -->
+            </div>
+        </div>
+    </div>
+    <!-- END FOOTER -->
+
+    <!-- BEGIN fast view of a product -->
+    <div id="product-pop-up" style="display: none; width: 700px;">
+        <div class="product-page product-pop-up">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-3">
+                    <div class="product-main-image">
+                        <img src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/temp/products/model7.jpg" alt="Cool green dress with red bell" class="img-responsive">
+                    </div>
+                    <div class="product-other-images">
+                        <a href="#" class="active"><img alt="Berry Lace Dress" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/temp/products/model3.jpg"></a>
+                        <a href="#"><img alt="Berry Lace Dress" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/temp/products/model4.jpg"></a>
+                        <a href="#"><img alt="Berry Lace Dress" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/temp/products/model5.jpg"></a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-9">
+                    <h1>Cool green dress with red bell</h1>
+                    <div class="price-availability-block clearfix">
+                        <div class="price">
+                            <strong><span>$</span>47.00</strong>
+                            <em>$<span>62.00</span></em>
+                        </div>
+                        <div class="availability">
+                            Availability: <strong>In Stock</strong>
+                        </div>
+                    </div>
+                    <div class="description">
+                        <p>Lorem ipsum dolor ut sit ame dolore  adipiscing elit, sed nonumy nibh sed euismod laoreet dolore magna aliquarm erat volutpat
+                            Nostrud duis molestie at dolore.</p>
+                    </div>
+                    <div class="product-page-options">
+                        <div class="pull-left">
+                            <label class="control-label">Size:</label>
+                            <select class="form-control input-sm">
+                                <option>L</option>
+                                <option>M</option>
+                                <option>XL</option>
+                            </select>
+                        </div>
+                        <div class="pull-left">
+                            <label class="control-label">Color:</label>
+                            <select class="form-control input-sm">
+                                <option>Red</option>
+                                <option>Blue</option>
+                                <option>Black</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="product-page-cart">
+                        <div class="product-quantity">
+                            <input id="product-quantity" type="text" value="1" readonly name="product-quantity" class="form-control input-sm">
+                        </div>
+                        <button class="btn btn-primary" type="submit">Add to cart</button>
+                        <button class="btn btn-default" type="submit">More details</button>
+                    </div>
+                </div>
+
+                <div class="sticker sticker-sale"></div>
+            </div>
+        </div>
+    </div>
+    <!-- END fast view of a product -->
+
+    <!-- Load javascripts at bottom, this will reduce page load time -->
+    <!-- BEGIN CORE PLUGINS (REQUIRED FOR ALL PAGES) -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/respond.min.js"></script>
+    <![endif]-->
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/back-to-top.js"></script>
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/jQuery-slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- END CORE PLUGINS -->
+
+    <!-- BEGIN PAGE LEVEL JAVASCRIPTS (REQUIRED ONLY FOR CURRENT PAGE) -->
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/fancybox/source/jquery.fancybox.pack.js"></script><!-- pop up -->
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/bxslider/jquery.bxslider.min.js"></script><!-- slider for products -->
+    <script type="text/javascript" src='<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/zoom/jquery.zoom.min.js'></script><!-- product zoom -->
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/bootstrap-touchspin/bootstrap.touchspin.js" type="text/javascript"></script><!-- Quantity -->
+
+    <!-- BEGIN LayerSlider -->
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/layerslider/jQuery/jquery-easing-1.3.js" type="text/javascript"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/layerslider/jQuery/jquery-transit-modified.js" type="text/javascript"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/plugins/layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+    <!-- END LayerSlider -->
+
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/scripts/app.js"></script>
+    <script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+assets/scripts/index.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+            App.init();
+            App.initBxSlider();
+            Index.initLayerSlider();
+            App.initImageZoom();
+            App.initTouchspin();
+        });
+    </script>
+    <!-- END PAGE LEVEL JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html><?php }} ?>
---
+++ application/controllers/Admin/home_admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Windows 8.1X64 M1
+ * Date: 1/28/2015
+ * Time: 10:05 AM
+ */
+class Home_admin extends CI_Controller{
+    public function Home(){
+        parent::__construct();
+    }
+    public function index(){
+        echo 'aaaaa';
+    }
+}
