b9ebba77a3e7a68d9d51d0e87f5393f750aa84f4
vanchuong201 <butterfly184>
2015-02-04 22:45:10 +0700

    update assets

--- application/controllers/Admin/mngProducts.php
+++ application/controllers/Admin/mngProducts.php
@@ -10,21 +10,22 @@ class MngProducts extends CI_Controller{
         parent::__construct();
         $this->parser->assign('scripts','EcommerceProducts.init();');
         $this->parser->assign('jss',array(
-                                            'assets/scripts/custom/ecommerce-products.js',
-                                            'assets/scripts/core/datatable.js',
                                             'assets/plugins/select2/select2.min.js',
                                             'assets/plugins/data-tables/jquery.dataTables.js',
                                             'assets/plugins/data-tables/DT_bootstrap.js',
                                             'assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                                        ));
+                                            'assets/scripts/custom/ecommerce-products.js',
+                                            'assets/scripts/core/datatable.js',
+                                        )
+        );
     }
 
     public function index(){
 
 
         $this->parser->assign('breadcrumb3','Manage Products');
-        //$this->parser->parse('Admin/header.tpl');
+        $this->parser->parse('Admin/header.tpl');
         $this->parser->parse('Admin/MngProducts/products.tpl');
-        //$this->parser->parse('Admin/footer.tpl');
+        $this->parser->parse('Admin/footer.tpl');
     }
 }
